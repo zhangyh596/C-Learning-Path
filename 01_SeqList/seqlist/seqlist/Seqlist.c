c94e9ede0f659b634fbf340865aa7738b79f70f8
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-01-26 00:40:07 +0800

    顺序表的初始化

---
+++ 01_SeqList/seqlist/seqlist/Seqlist.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Seqlist.h"
+
+//Ë³Ðò±í³õÊ¼»¯
+void SLInit(SL* ps)
+{
+	ps->a = NULL;
+	ps->size = ps->capacity = 0;
+}
+
+
---
+++ 01_SeqList/seqlist/seqlist/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Seqlist.h"
+
+void SLTest01()
+{
+	SL s1;
+	SLInit(&s1);
+}
+
+int main()
+{
+	SLTest01();
+	return 0;
+}
