546d12464998d03f05ce6e6e44e87c2ac4824d19
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-01 00:12:27 +0800

    顺序表头文件

--- 02_Contact/Contact/Contact/Seqlist.h
+++ 02_Contact/Contact/Contact/Seqlist.h
@@ -9,6 +9,18 @@ typedef int SLDataType;
 typedef struct Seqlist
 {
 	SLDataType* a;
-	int size;//数据个数
+	int size;//有效数据个数
 	int capacity;//空间容量
-};
+}SL;
+
+void SLInit(SL* ps);
+void SLDesttoy(SL* ps);
+void SLPrint(SL s);
+void SLCheckCapacity(SL* ps);
+void SLPushBack(SL* ps, SLDataType x);
+void SLPushFront(SL* ps, SLDataType x);
+void SLPopBack(SL* ps);
+void SLPopFront(SL* ps);
+void SLInsert(SL* ps, int pos, SLDataType x);
+void SLErase(SL* ps, int pos);
+int SLFind(SL* ps, SLDataType x);
