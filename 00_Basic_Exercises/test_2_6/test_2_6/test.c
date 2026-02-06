413c739a42d396668c0a11952368a862fe4e4f6a
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-07 00:20:14 +0800

    力扣链表题训练

--- 00_Basic_Exercises/test_2_6/test_2_6/test.c
+++ 00_Basic_Exercises/test_2_6/test_2_6/test.c
@@ -8,6 +8,51 @@ struct ListNode
 	struct ListNode* next;
 };
 
+struct ListNode* middleNode(struct ListNode* head) {
+	struct ListNode* slow = head;
+	struct ListNode* fast = head;
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return slow;
+}
+
+struct ListNode* reverseList(struct ListNode* head) {
+	if (head == NULL)
+	{
+		return head;
+	}
+	struct ListNode* n1;
+	struct ListNode* n2;
+	struct ListNode* n3;
+	n1 = NULL, n2 = head, n3 = n2->next;
+	while (n2)
+	{
+		n2->next = n1;
+		n1 = n2;
+		n2 = n3;
+		if (n3 != NULL)
+		{
+			n3 = n3->next;
+		}
+	}
+	return n1;
+}
+
+struct ListNode* reverseList(struct ListNode* head) {
+	struct ListNode* prev = NULL;
+	struct ListNode* pcur = head;
+	while (pcur)
+	{
+		struct ListNode* next = pcur->next;
+		pcur->next = prev;
+		prev = pcur;
+		pcur = next;
+	}
+	return prev;
+}
 
 struct ListNode* removeElements(struct ListNode* head, int val) {
 	struct ListNode* newhead = NULL, * newtail = NULL;
