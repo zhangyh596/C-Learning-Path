f43c002416fd8d21ba959eb75b1c8f743a992db5
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-09 11:10:56 +0800

    单链表打印，创建新节点（初步实通讯录）

---
+++ 04_Contact_SList/Contact/Contact/SList.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "SList.h"
+
+//单链表打印
+void SListPrint(SListNode* phead)
+{
+	SListNode* pcur = phead;
+	while (pcur != NULL)
+	{
+		printf("%d->", pcur->data);
+		pcur = pcur->next;
+	}
+	printf("NULL\n");
+}
+
+//创建新节点
+SListNode* BuySListNode(SLTDataType x)
+{
+	SListNode* newNode = (SListNode*)malloc(sizeof(SListNode));
+	if (newNode == NULL)
+	{
+		perror("malloc fail");
+		exit(-1);
+	}
+	newNode->data = x;
+	newNode->next = NULL;
+	return newNode;
+}
+
