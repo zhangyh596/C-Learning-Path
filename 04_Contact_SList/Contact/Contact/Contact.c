907fbebdb2f5f7e2bd20d8ce3fc5d1d694734981
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-10 19:18:59 +0800

    通讯录初始化

---
+++ 04_Contact_SList/Contact/Contact/Contact.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Contact.h"
+#include "SList.h"
+#include <string.h>
+
+//辅助函数（通过名字寻找节点）
+static Contact* FindByName(Contact* pc, char* name)
+{
+	Contact* pcur = pcon;
+	while (pcur)
+	{
+		if (strcmp(pcur->data.name, name) == 0)
+		{
+			return pcur;
+		}
+		pcur = pcur->next;
+	}
+	return NULL;
+}
+
+//通讯录初始化
+void ContactInit(Contact** ppcon)
+{
+	*ppcon = NULL;
+}
