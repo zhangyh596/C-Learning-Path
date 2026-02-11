2625f2c437b8dc584387a9486461524a57f12528
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-11 23:41:24 +0800

    双向链表初始化

---
+++ 05_DList/DList/DList/DList.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "DList.h"
+
+//辅助函数：创建新节点
+DListNode* BuyDListNode(DLTDataType x)
+{
+	DListNode* newnode = (DListNode*)malloc(sizeof(DListNode));
+	if (newnode == NULL)
+	{
+		perror("malloc fail");
+		exit(-1);
+	}
+	newnode->data = x;
+	newnode->next = NULL;
+	newnode->prev = NULL;
+}
+
+//初始化
+DListNode* DListInit()
+{
+	DListNode* phead = BuyDListNode(-1);
+	phead->next = phead;
+	phead->prev = phead;
+}
