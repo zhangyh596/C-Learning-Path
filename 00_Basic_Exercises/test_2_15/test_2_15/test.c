49f17edcf4b05eb771ee46495c8bb2c5e294a46d
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-15 18:33:53 +0800

    2026年2月15日博客

---
+++ 00_Basic_Exercises/test_2_15/test_2_15/test.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int int_max(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+//
+//float float_max(float x, float y)
+//{
+//	return x > y ? x : y;
+//}
+//
+#define GENERIC_MAX(type) \
+type type##_max(type x, type y) \
+{                               \
+      return (x>y?x:y);         \
+}
+
+GENERIC_MAX(int)
+GENERIC_MAX(float)
+
+int main()
+{
+	int a = int_max(2, 3);
+	printf("%d", a);
+	return 0;
+}
