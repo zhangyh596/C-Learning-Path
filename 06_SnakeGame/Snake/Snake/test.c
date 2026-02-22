bf57d2cefc640c6b1f68c747df5a84c3bcf714ac
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-23 00:21:59 +0800

    贪吃蛇大致逻辑

---
+++ 06_SnakeGame/Snake/Snake/test.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <locale.h>
+#include "snake.h"
+
+//完成的是游戏的测试逻辑
+void test()
+{
+	//创建贪吃蛇
+	Snake snake = { 0 };
+	//初始化游戏
+	//1.打印环境界面
+	//2.功能介绍
+	//3.绘制地图
+	//4.创建蛇
+	//5.创建食物
+	//6.设置游戏的相关信息
+	GameStart(&snake);
+
+	//运行游戏
+	
+	//结束游戏（善后工作）
+}
+
+int main()
+{
+	//设置适配本地环境
+	set_locale(LC_ALL, "");
+
+	test();
+	return 0;
+}
