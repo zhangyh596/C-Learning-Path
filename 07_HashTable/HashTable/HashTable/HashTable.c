235dbb2623de45461ac3d12a6a344ee5e4a64a40
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-11 09:22:23 +0800

    哈希函数

--- 07_HashTable/HashTable/HashTable/HashTable.c
+++ 07_HashTable/HashTable/HashTable/HashTable.c
@@ -2,3 +2,18 @@
 
 #include "HashTable.h"
 
+unsigned int hash(const char* key, int table_size)
+{
+	unsigned long int value = 5381;//魔法初始数字
+	unsigned int i = 0;
+	unsigned int key_len = strlen(key);
+
+	//核心
+	for (int i = 0; i < key_len; i++)
+	{
+		value = value * 33 + key[i];
+	}
+
+	//挤进柜子里面
+	return value % table_size;
+}
