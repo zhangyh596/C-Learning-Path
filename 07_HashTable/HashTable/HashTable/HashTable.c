ab0c361584006dc8bdcb58766a3fd6fc2a6824d9
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-11 11:06:52 +0800

    插入键值对

--- 07_HashTable/HashTable/HashTable/HashTable.c
+++ 07_HashTable/HashTable/HashTable/HashTable.c
@@ -2,6 +2,25 @@
 
 #include "HashTable.h"
 
+//辅助函数（创建一个新节点）
+Node* create_node(const char* key, int value)
+{
+	//做一个空袋子
+	Node* new_node = (Node*)malloc(sizeof(Node));
+
+	//准备写上名字
+	new_node->key = (char*)malloc(strlen(key) + 1);
+	strcpy(new_node->key, key);
+
+	//把钱放进去
+	new_node->value = value;
+
+	//袋子下面的钩子暂时没有东西
+	new_node->next = NULL;
+
+	return new_node;
+}
+
 unsigned int hash(const char* key, int table_size)
 {
 	unsigned long int value = 5381;//魔法初始数字
@@ -35,4 +54,32 @@ HashTable* create_table(int size)
 
 	//结束
 	return ht;
+}
+
+void insert(HashTable* ht, const char* key, int value)
+{
+	//询问前台去几号抽屉
+	unsigned int slot = hash(key, ht->size);
+
+	//走到那个抽屉看看现在的挂钩挂着什么
+	Node* entry = ht->entries[slot];
+
+	//顺着挂钩往下翻看看有没有同一个名字的（处理冲突/更新）
+	while (entry)
+	{
+		if (strcmp(entry->key, key) == 0)
+		{
+			//找到同名的袋子
+			entry->value = value;//直接更新值
+			return;
+		}
+		entry = entry->next;
+	}
+
+	//翻到底都没有同名的，那就拿一个新袋子
+	Node* new_node = create_node(key, value);
+
+	//采用头插法，尾插还得遍历到尾节点和分类讨论
+	new_node->next = ht->entries[slot];
+	ht->entries[slot] = new_node;
 }
