1b6272d726d1203495821f90d768eb53d51879e7
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-11 09:12:36 +0800

    哈希表初定义

---
+++ 07_HashTable/HashTable/HashTable/HashTable.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <stdio.h>
+
+typedef struct Node
+{
+	char* key;//你的名字（比如“apple”）
+	int value;//你的存款（比如100）
+	struct Node* next;//重点！用来挂下一个袋子
+}Node;
+
+typedef struct HashTable
+{
+	int size;//记录柜子一共有多少个抽屉
+	Node** enties;//重点！这是抽屉的集合
+}HashTable;
