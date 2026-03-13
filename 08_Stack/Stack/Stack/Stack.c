8a5c2ad1101cef1180301f0a7d9583a93c1bae5d
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-13 22:45:13 +0800

    栈的初始化

--- 08_Stack/Stack/Stack/Stack.c
+++ 08_Stack/Stack/Stack/Stack.c
@@ -1,1 +1,19 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Stack.h"
+
+// s 代表我们要操作的那个栈（通过指针传进来）
+// capacity 是用户告诉我们，这个栈要多大
+void stackInit(Stack* s, int capacity)
+{
+	s->data = (int*)malloc(capacity * sizeof(int));
+
+	if (s->data == NULL)
+	{
+		perror("malloc failed");
+		exit(1);//强行退出
+	}
+
+	s->top = -1;//说明现在是空的
+	s->capacity = capacity;//记住最大容量
+}
