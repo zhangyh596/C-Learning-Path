1ad665eb220d2078153891b9da808659e00da062
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-17 09:23:33 +0800

    环形队列初始化

--- 10_CircularQueue/CircularQueue/CircularQueue/CircularQueue.c
+++ 10_CircularQueue/CircularQueue/CircularQueue/CircularQueue.c
@@ -1,1 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "CircularQueue.h"
+
+void queueInit(CircularQueue* q, int maxSize)
+{
+	//【核心心法】：你想装 maxSize 个人，我底层必须开辟 maxSize + 1 个位置！
+	//那个多出来的位置，就是为了防止 front 追上 rear 时分不清是空还是满。
+	q->capacity = maxSize + 1;
+
+	//此时malloc一块空间之后不会再用malloc
+	q->data = (int*)malloc(sizeof(int) * q->capacity);
+	if (q->data == NULL)
+	{
+		perror("malloc failed");
+		return;
+	}
+
+	q->front = 0;
+	q->rear = 0;
+
+	printf("环形队列初始化成功，真实容量为%d，可装载人数为%d\n", q->capacity, maxSize);
+}
--- 10_CircularQueue/CircularQueue/CircularQueue/test.c
+++ 10_CircularQueue/CircularQueue/CircularQueue/test.c
@@ -1,1 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "CircularQueue.h"
+
+int main()
+{
+	CircularQueue MyQueue;
+	queueInit(&MyQueue, 5);
+	return 0;
+}
