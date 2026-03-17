b2e07a145f5d50722ec4712bf77680661178c93b
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-17 09:05:12 +0800

    定义环形队列的结构体

---
+++ 10_CircularQueue/CircularQueue/CircularQueue/CircularQueue.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct CircularQueue
+{
+	int* data;//待会用malloc获取一块连续的数字空间
+	int capacity;//记录这个队列的总容量
+	int front;//队头游标，排在最前面的数据
+	int rear;//队尾游标，永远指向下一个空位
+}CircularQueue;
+
+//初始化环形队列
+void queueInit(CircularQueue* q, int maxSize);
