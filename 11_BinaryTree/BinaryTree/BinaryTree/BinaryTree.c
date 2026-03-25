e4a24f56979f8d58b7ebfd4877b93cf240df69a4
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-25 15:53:09 +0800

    创建新节点

--- 11_BinaryTree/BinaryTree/BinaryTree/BinaryTree.c
+++ 11_BinaryTree/BinaryTree/BinaryTree/BinaryTree.c
@@ -1,1 +1,22 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "BinaryTree.h"
+
+TreeNode* createNode(int value)
+{
+	TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
+
+	//检查是否malloc成功
+	if (newNode == NULL)
+	{
+		perror("malloc failed");
+		exit(1);//退出程序
+	}
+
+	//正式初始化节点
+	newNode->data = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return newNode;
+}
