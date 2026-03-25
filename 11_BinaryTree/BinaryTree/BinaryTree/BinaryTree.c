5a4d12760e6c025b9da83c3c0f2a755f39d665e2
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-25 16:02:42 +0800

    插入节点

--- 11_BinaryTree/BinaryTree/BinaryTree/BinaryTree.c
+++ 11_BinaryTree/BinaryTree/BinaryTree/BinaryTree.c
@@ -20,3 +20,26 @@ TreeNode* createNode(int value)
 
 	return newNode;
 }
+
+TreeNode* insertNode(TreeNode* root, int value)
+{
+	if (root == NULL)
+	{
+		return createNode(value);
+	}
+
+	// 如果新来的值比当前节点的数据小，说明它该去左边
+	if (value < root->data)
+	{
+		// 让当前节点的左指针，接住从左子树传回来的结果
+		root->left = insertNode(root->left, value);
+	}
+	// 如果新来的值比当前节点的数据大，说明它该去右边
+	else if (value > root->data)
+	{
+		// 让当前节点的右指针，接住从右子树传回来的结果
+		root->right = insertNode(root->right, value);
+	}
+	// 如果新值等于当前节点的值，我们一般什么都不做，因为二叉搜索树通常不存重复数据
+	return root;
+}
