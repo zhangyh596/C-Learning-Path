51c1f62fa5037f65fe75993a9bcdff8fc65d7564
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-25 15:41:54 +0800

    初步定义二叉树的结构体

---
+++ 11_BinaryTree/BinaryTree/BinaryTree/BinaryTree.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode
+{
+	int data;//数据域：用来存东西
+	struct TreeNode* left;//指针域：指向左边子节点的指针
+	struct TreeNOde* right;//指针域：指向右边子节点的指针
+}TreeNode;
