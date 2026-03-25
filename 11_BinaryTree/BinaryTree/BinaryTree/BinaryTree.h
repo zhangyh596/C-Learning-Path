3fdc7a40bc05daf62a8d5971e5b58b04a2c9cbcd
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-25 16:41:52 +0800

    bug修复完成

--- 11_BinaryTree/BinaryTree/BinaryTree/BinaryTree.h
+++ 11_BinaryTree/BinaryTree/BinaryTree/BinaryTree.h
@@ -7,7 +7,7 @@ typedef struct TreeNode
 {
 	int data;//数据域：用来存东西
 	struct TreeNode* left;//指针域：指向左边子节点的指针
-	struct TreeNOde* right;//指针域：指向右边子节点的指针
+	struct TreeNode* right;//指针域：指向右边子节点的指针
 }TreeNode;
 
 //创建新节点
