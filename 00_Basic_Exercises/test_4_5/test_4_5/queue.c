0034a0976ca5fbd7f8639e6536070924b9c95506
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-08 00:00:12 +0800

    2026年4月5日学习

---
+++ 00_Basic_Exercises/test_4_5/test_4_5/queue.c
@@ -0,0 +1,86 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "queue.h"
+
+NodeQueue* queue_create(int capacity)
+{
+	NodeQueue* queue = (NodeQueue*)malloc(sizeof(NodeQueue));
+	if (queue == NULL)
+	{
+		perror("malloc fail");
+		return NULL;
+	}
+	queue->capacity = capacity + 1;
+
+	queue->data = (Node*)malloc(sizeof(Node) * queue->capacity);
+	if (queue->data == NULL)
+	{
+		perror("malloc fail");
+		return NULL;
+	}
+
+	queue->head = 0;
+	queue->tail = 0;
+
+	return queue;
+}
+
+bool queueIsEmpty(NodeQueue* q)
+{
+	if (q->head == q->tail)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool queueIsFull(NodeQueue* q)
+{
+	if ((q->tail + 1) % q->capacity == q->head)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool queue_push(NodeQueue* q, Node value)
+{
+	if (queueIsFull(q))
+	{
+		return false;
+	}
+
+	q->data[q->tail] = value;
+	q->tail = (q->tail + 1) % q->capacity;
+	return true;
+}
+
+bool queue_pop(NodeQueue* q, Node* out_value)
+{
+	if (q == NULL || out_value == NULL || queueIsEmpty(q))
+	{
+		return false;
+	}
+
+	*out_value = q->data[q->head];
+
+	q->head = (q->head + 1) % q->capacity;
+	return true;
+}
+
+void queueDestroy(NodeQueue* q)
+{
+	if (q->data != NULL)
+	{
+		free(q->data);
+		q->data = NULL;
+	}
+
+	free(q);
+}
---
+++ 00_Basic_Exercises/test_4_5/test_4_5/queue.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct
+{
+	int x;
+	int y;
+	//int step;
+}Node;
+
+typedef struct
+{
+	Node* data;      // 指向动态分配的连续内存
+	int head;       // 队头索引
+	int tail;       // 队尾索引（指向下一个可插入的位置）
+	int capacity;   // 队列的最大容量
+} NodeQueue;
+
+// 你需要实现的接口：
+NodeQueue* queue_create(int capacity);
+bool queueIsEmpty(NodeQueue* q);
+bool queueIsFull(NodeQueue* q);
+bool queue_push(NodeQueue* q, Node value);
+bool queue_pop(NodeQueue* q, Node* out_value);
+void queueDestroy(NodeQueue* q);
