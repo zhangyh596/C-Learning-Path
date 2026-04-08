581be0c22c3a6346e56a0cf44cee2d6d67da124c
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-08 14:48:00 +0800

    机考模拟第四题

---
+++ 00_Basic_Exercises/Project1/Project1/test4.c
@@ -0,0 +1,29 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//#include <limits.h>
+//
+//int main()
+//{
+//	char s[1005];
+//	scanf("%s", s);
+//
+//	long long res = 0;
+//
+//	for (int i = 0; s[i] != '\0'; i++)
+//	{
+//		if (s[i] >= '0' && s[i] <= '9')
+//		{
+//			int digit = s[i] - '0';
+//			res = res * 10 + digit;
+//
+//			if (res > INT_MAX)
+//			{
+//				printf("OVERFLOW\n");
+//				return 0;
+//			}
+//		}
+//	}
+//	printf("%lld\n", res);
+//	return 0;
+//}
