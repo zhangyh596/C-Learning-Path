9336f904816ff968ed31a4da6614fefa73554353
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-08 14:47:05 +0800

    机考模拟第二题

---
+++ 00_Basic_Exercises/Project1/Project1/test2.c
@@ -0,0 +1,36 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//enum Color
+//{
+//	red,
+//	green,
+//	yellow
+//};
+//
+//void print_color(enum Color c)
+//{
+//	switch (c)
+//	{
+//	case red:
+//		printf("red\n");
+//		break;
+//	case green:
+//		printf("green\n");
+//		break;
+//	case yellow:
+//		printf("yellow\n");
+//		break;
+//	default:
+//		printf("unknown color\n");
+//		break;
+//	}
+//}
+//
+//int main()
+//{
+//	print_color(green);
+//	print_color(red);
+//	return 0;
+//}
