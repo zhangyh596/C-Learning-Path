b603be999f3afb8c612282790c468cc0d9e204ca
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-19 16:05:52 +0800

    2026年4月15日学习

---
+++ 00_Basic_Exercises/test_4_15/test_4_15/Queue.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node
+{
+	int data;
+	struct Node* next;
+}Node;
+
+typedef struct Queue
+{
+	Node* front;
+	Node* rear;
+}Queue;
+
+void queueInit(Queue* q);
+bool queueIsEmpty(Queue* q);
+Node* createNode(int val);
+void queuePush(Queue* q, int val);
+int queuePop(Queue* q);
+void queueDestroy(Queue* q);
