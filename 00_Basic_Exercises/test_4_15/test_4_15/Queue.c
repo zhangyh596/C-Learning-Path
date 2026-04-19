a2bfb807277d665a7230775b6656a63fa079956f
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-19 16:05:26 +0800

    2026年4月15日学习

---
+++ 00_Basic_Exercises/test_4_15/test_4_15/Queue.c
@@ -0,0 +1,78 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Queue.h"
+
+void queueInit(Queue* q)
+{
+	q->front = NULL;
+	q->rear = NULL;
+}
+
+bool queueIsEmpty(Queue* q)
+{
+	if (q->front == NULL)
+	{
+		return true;
+	}
+	return false;
+}
+
+Node* createNode(int val)
+{
+	Node* newnode = (Node*)malloc(sizeof(Node));
+	if (newnode == NULL)
+	{
+		perror("malloc fail");
+		return NULL;
+	}
+
+	newnode->data = val;
+	newnode->next = NULL;
+
+	return newnode;
+}
+
+void queuePush(Queue* q, int val)
+{
+	Node* newnode = createNode(val);
+
+	if (queueIsEmpty(q))
+	{
+		q->front = q->rear = newnode;
+	}
+	else
+	{
+		q->rear->next = newnode;
+		q->rear = newnode;
+	}
+}
+
+int queuePop(Queue* q)
+{
+	if (queueIsEmpty(q))
+	{
+		printf("队列为空，出队失败\n");
+		return -1;
+	}
+
+	Node* popNode = q->front;
+	int popVal = popNode->data;
+
+	q->front = q->front->next;
+	if (q->front == NULL)
+	{
+		q->rear = NULL;
+	}
+
+	free(popNode);
+
+	return popVal;
+}
+
+void queueDestroy(Queue* q)
+{
+	while (!queueIsEmpty(q))
+	{
+		queuePop(q);
+	}
+}
