1b342a80dbc24093d5e93f58bf3a14d7845dfaa4
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-19 16:06:58 +0800

    2026年4月15日学习

---
+++ 00_Basic_Exercises/test_4_15/test_4_15/test.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include "Stack.h"
+//
+//int main()
+//{
+//	Stack myStack;
+//	stackInit(&myStack, 3);
+//
+//	stackPush(&myStack, 1);
+//	stackPush(&myStack, 2);
+//	stackPush(&myStack, 3);
+//	stackPush(&myStack, 4);
+//
+//	stackPop(&myStack);
+//
+//	stackDestroy(&myStack);
+//	return 0;
+//}
+
+#include "Queue.h"
+
+int main()
+{
+	Queue myQueue;
+	queueInit(&myQueue);
+
+	queuePush(&myQueue, 1);
+	queuePush(&myQueue, 2);
+	queuePush(&myQueue, 3);
+	queuePush(&myQueue, 4);
+
+	int val = queuePop(&myQueue);
+	printf("%d\n", val);
+
+	queueDestroy(&myQueue);
+	return 0;
+}
