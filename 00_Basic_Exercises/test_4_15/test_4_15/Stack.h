1207e496a44063c038c0da6dbe0702c29a13a84d
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-19 16:06:36 +0800

    2026年4月15日学习

---
+++ 00_Basic_Exercises/test_4_15/test_4_15/Stack.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Stack
+{
+	int capacity;
+	int top;
+	int* data;
+}Stack;
+
+void stackInit(Stack* s, int capacity);
+bool stackIsFull(Stack* s);
+void stackPush(Stack* s, int val);
+bool stackIsEmpty(Stack* s);
+int stackPop(Stack* s);
+void stackDestroy(Stack* s);
