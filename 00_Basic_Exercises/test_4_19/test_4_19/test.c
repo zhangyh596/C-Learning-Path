aad58f8b72bd797142864677981cdbd4d1ad10ef
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-20 18:01:49 +0800

    2026年4月19日学习

---
+++ 00_Basic_Exercises/test_4_19/test_4_19/test.c
@@ -0,0 +1,59 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+	int val;
+	struct Node* next;
+	struct Node* random;
+};
+
+struct Node* copyRandomList(struct Node* head) {
+	if (head == NULL)
+	{
+		return NULL;
+	}
+
+	struct Node* pcur = head;
+	while (pcur)
+	{
+		struct Node* next = pcur->next;
+		struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
+		newnode->val = pcur->val;
+
+		newnode->next = next;
+		pcur->next = newnode;
+		pcur = next;
+	}
+
+	pcur = head;
+	while (pcur)
+	{
+		if (pcur->random == NULL)
+		{
+			pcur->next->random = NULL;
+		}
+		else
+		{
+			pcur->next->random = pcur->random->next;
+		}
+		pcur = pcur->next->next;
+	}
+
+	pcur = head;
+	struct Node* newhead = head->next;
+	struct Node* newpcur = newhead;
+	while (pcur)
+	{
+		pcur->next = pcur->next->next;
+		if (newpcur->next != NULL)
+		{
+			newpcur->next = newpcur->next->next;
+		}
+
+		pcur = pcur->next;
+		newpcur = newpcur->next;
+	}
+	return newhead;
+}
