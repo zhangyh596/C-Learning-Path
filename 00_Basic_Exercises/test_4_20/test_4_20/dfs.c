a048df8dccb3b7deadbf23c70591929e743f1c2a
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-20 18:02:45 +0800

    2026年4月20日学习

---
+++ 00_Basic_Exercises/test_4_20/test_4_20/dfs.c
@@ -0,0 +1,61 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "dfs.h"
+
+void initStack(Stack* s, int initial_cap)
+{
+	s->data = (Point*)malloc(sizeof(Point) * initial_cap);
+	if (s->data == NULL)
+	{
+		return;
+	}
+
+	s->top = -1;
+	s->capacity = initial_cap;
+}
+
+bool isEmptyStack(Stack* s)
+{
+	if (s->top == -1)
+	{
+		return true;
+	}
+	return false;
+}
+
+void pushStack(Stack* s, Point p)
+{
+	if (s->top + 1 == s->capacity)
+	{
+		int newCapacity = s->capacity == 0 ? 4 : s->capacity * 2;
+		Point* tmp = (Point*)realloc(s->data, sizeof(Point) * newCapacity);
+		if (tmp == NULL)
+		{
+			return;
+		}
+
+		s->data = tmp;
+		s->capacity = newCapacity;
+	}
+
+	s->top++;
+	s->data[s->top] = p;
+}
+
+Point popStack(Stack* s)
+{
+	Point p = s->data[s->top];
+	s->top--;
+	return p;
+}
+
+void freeStack(Stack* s)
+{
+	if (s->data != NULL)
+	{
+		free(s->data);
+		s->data = NULL;
+	}
+	s->top = -1;
+	s->capacity = 0;
+}
