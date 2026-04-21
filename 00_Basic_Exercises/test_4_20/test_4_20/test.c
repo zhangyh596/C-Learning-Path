d4d30f2b663803e9b61d1c89ecc17631a332f6bc
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-21 10:02:26 +0800

    dfs静态版本

--- 00_Basic_Exercises/test_4_20/test_4_20/test.c
+++ 00_Basic_Exercises/test_4_20/test_4_20/test.c
@@ -1,26 +1,85 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1
 
-#include "dfs.h"
+//#include "dfs.h"
+//
+//int dx[4] = { -1, 1, 0, 0 };
+//int dy[4] = { 0, 0, -1, 1 };
+//
+//int dfs(int start_x, int start_y, int target_x, int target_y)
+//{
+//	int vis[100][100] = { 0 };
+//	Stack s;
+//	initStack(&s, 100);
+//
+//	Point start = { start_x, start_y };
+//	pushStack(&s, start);
+//	vis[start_x][start_y] = 1;
+//
+//	while (!isEmptyStack(&s))
+//	{
+//		Point curr = popStack(&s);
+//		if (curr.x == target_x && curr.y == target_y)
+//		{
+//			freeStack(&s);
+//			return 1;
+//		}
+//
+//		for (int i = 0; i < 4; i++)
+//		{
+//			int nx = curr.x + dx[i];
+//			int ny = curr.y + dy[i];
+//
+//			if (nx >= 0 && nx < 100 && ny >= 0 && ny < 100 && vis[nx][ny] == 0)
+//			{
+//				vis[nx][ny] = 1;
+//				Point newPoint = { nx, ny };
+//				pushStack(&s, newPoint);
+//			}
+//		}
+//	}
+//	freeStack(&s);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	if (dfs(0, 0, 4, 4) == 1)
+//	{
+//		printf("有路径\n");
+//	}
+//	return 0;
+//}
+
+
+#include <stdio.h>
 
 int dx[4] = { -1, 1, 0, 0 };
 int dy[4] = { 0, 0, -1, 1 };
 
+typedef struct Point
+{
+	int x;
+	int y;
+}Point;
+
+Point stack[100000];
+int top = 0;
+
 int dfs(int start_x, int start_y, int target_x, int target_y)
 {
 	int vis[100][100] = { 0 };
-	Stack s;
-	initStack(&s, 100);
+	top = 0;
 
 	Point start = { start_x, start_y };
-	pushStack(&s, start);
+	stack[top++] = start;
 	vis[start_x][start_y] = 1;
 
-	while (!isEmptyStack(&s))
+	while (top > 0)
 	{
-		Point curr = popStack(&s);
+		Point curr = stack[--top];
+
 		if (curr.x == target_x && curr.y == target_y)
 		{
-			freeStack(&s);
 			return 1;
 		}
 
@@ -29,23 +88,26 @@ int dfs(int start_x, int start_y, int target_x, int target_y)
 			int nx = curr.x + dx[i];
 			int ny = curr.y + dy[i];
 
-			if (nx >= 0 && nx < 100 && ny >= 0 && ny < 100 && vis[nx][ny] == 0)
+			if (nx < 100 && nx >= 0 && ny < 100 && ny >= 0 && vis[nx][ny] == 0)
 			{
 				vis[nx][ny] = 1;
 				Point newPoint = { nx, ny };
-				pushStack(&s, newPoint);
+				stack[top++] = newPoint;
 			}
 		}
 	}
-	freeStack(&s);
 	return 0;
 }
 
 int main()
 {
-	if (dfs(0, 0, 4, 4) == 1)
+	if (dfs(0, 0, 4, 4))
 	{
 		printf("有路径\n");
+	}
+	else
+	{
+		printf("没有路径\n");
 	}
 	return 0;
 }
