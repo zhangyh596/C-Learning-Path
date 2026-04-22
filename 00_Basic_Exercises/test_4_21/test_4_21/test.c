e4a095ce6705ac2be6bf593469c7e20dce5e2568
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-22 10:14:33 +0800

    2026年4月21日学习

---
+++ 00_Basic_Exercises/test_4_21/test_4_21/test.c
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int cmp(const void* a, const void* b)
+{
+	const char* str1 = (const char*)a;
+	const char* str2 = (const char*)b;
+
+	return strcmp(str1, str2);
+}
+
+int main()
+{
+	/*char word[100][100];
+	int n = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%s", word[i]);
+	}
+
+	qsort(word, n, sizeof(word[0]), cmp);
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%s ", word[i]);
+	}*/
+
+	char line[] = "12345,99,100000";
+	char word[100][100];
+	int count = 0;
+
+	char* token = strtok(line, ",");
+	while (token != NULL)
+	{
+		strcpy(word[count], token);
+		count++;
+		token = strtok(NULL, ",");
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		printf("%s ", word[i]);
+	}
+	return 0;
+}
