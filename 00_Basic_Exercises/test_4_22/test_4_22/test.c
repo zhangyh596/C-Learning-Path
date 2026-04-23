69b3960e16db1fc44eb1ab93ceb8e789d843f3df
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-23 23:36:25 +0800

    2026年4月22日学习

---
+++ 00_Basic_Exercises/test_4_22/test_4_22/test.c
@@ -0,0 +1,84 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+	int val;
+	struct ListNode* next;
+};
+
+//struct ListNode* reverseKGroup(struct ListNode* head, int k)
+//{
+//	struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+//	dummy->next = head;
+//
+//	struct ListNode* prev = dummy;
+//	struct ListNode* pcur = dummy->next;
+//	
+//	int count = 0;
+//	while (pcur)
+//	{
+//		pcur = pcur->next;
+//		count++;
+//	}
+//
+//	pcur = dummy->next;
+//	for (int i = 0; i < count / k; i++)
+//	{
+//		struct ListNode* groupPrev = NULL;
+//		struct ListNode* start = pcur;
+//
+//		for (int j = 0; j < k; j++)
+//		{
+//			struct ListNode* next = pcur->next;
+//			pcur->next = groupPrev;
+//			groupPrev = pcur;
+//			pcur = next;
+//		}
+//
+//		start->next = pcur;
+//		prev->next = groupPrev;
+//
+//		prev = start;
+//	}
+//	return dummy->next;
+//}
+
+
+struct ListNode* reverseKGroup(struct ListNode* head, int k)
+{
+	struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+	dummy->next = head;
+
+	struct ListNode* prev = dummy;
+	struct ListNode* pcur = dummy->next;
+
+	int count = 0;
+	while (pcur)
+	{
+		count++;
+		pcur = pcur->next;
+	}
+
+	pcur = dummy->next;
+	for (int i = 0; i < count / k; i++)
+	{
+		struct ListNode* groupPrev = NULL;
+		struct ListNode* start = pcur;
+
+		for (int j = 0; j < k; j++)
+		{
+			struct ListNode* next = pcur->next;
+			pcur->next = groupPrev;
+			groupPrev = pcur;
+			pcur = next;
+		}
+
+		prev->next = groupPrev;
+		start->next = pcur;
+
+		prev = start;
+	}
+	return dummy->next;
+}
