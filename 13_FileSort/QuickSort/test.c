14e024bd2c87113fc85a682bf3b16e201403d804
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-08-02 15:33:16 +0800

    快速排序：左右指针法

--- 13_FileSort/QuickSort/test.c
+++ 13_FileSort/QuickSort/test.c
@@ -70,12 +70,50 @@ int partition_lomuto(int *a, int low, int high)
     return i + 1; // 返回 Pivot 最终坐完之后的位置下标
 }
 
+// 方法三：左右指针法
+// 需要用到前面的swap函数
+int partition_hoare(int *a, int low, int high)
+{
+    int pivot = a[low]; // 选最左边的元素作为基准值
+    int left = low;
+    int right = high;
+
+    while (left < right)
+    {
+        // 必须right先走，找比pivot小的数
+        // 因为最后交换的时候是把相遇的位置和基准值交换，这个相遇的位置的值必须比pivot小
+        // 第一种情况：right 走着走着，主动撞上了停着的 left，下标为left的位置一定比pivot小
+        // 第二种情况：left 走着走着，主动撞上了停着的 right，说明此时下标为right的值已经比pivot小了
+        // 而如果让left先走，相遇的位置一定是一个比pivot大的数
+        // 必须写a[right]>=pivot而不能写a[right]>pivot，否则可能陷入死循环（当下标为left和下标为right都等于pivot）
+        while (left < right && a[right] >= pivot)
+        {
+            right--;
+        }
+
+        // left找比pivot大的数
+        while (left < right && a[left] <= pivot)
+        {
+            left++;
+        }
+
+        if (left < right)
+        {
+            swap(&a[left], &a[right]);
+        }
+    }
+
+    swap(&a[low], &a[left]);
+
+    return left;
+}
+
 void QuickSort(int *a, int low, int high)
 {
     // 当长度<=1不会进入
     if (low < high)
     {
-        int pi = partition_lomuto(a, low, high);
+        int pi = partition_hoare(a, low, high);
 
         // 递归实现左半边排序和右半边排序
         QuickSort(a, low, pi - 1);
